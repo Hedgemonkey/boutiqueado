a056e5b97f36b32d334ce39edcf4c7f7ce256baa
Hedgemonkey <hedgemonkey@gmail.com>
2025-02-06 03:24:51 +0000

    More debugging

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('test_s3/', views.test_s3_connection, name='test_s3_connection'),
+    path('upload_static/', views.upload_static_to_s3, name='upload_static_to_s3'),
 ]
